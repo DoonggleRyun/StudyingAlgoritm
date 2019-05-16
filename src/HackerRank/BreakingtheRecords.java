401a1a06b6c4b3aa1f38e7e834d975e599bbe0d9
devSryn <devseryun@gmail.com>
2019-05-16 18:07:14 +0900

    Breaking the Records

--- src/HackerRank/BreakingtheRecords.java
+++ src/HackerRank/BreakingtheRecords.java
@@ -2,9 +2,21 @@ package hackerRank;
 
 public class BreakingtheRecords {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    static int[] getRecord(int[] s){
+        int max = s[0];
+        int min = s[0];
+        int ans[] = new int[2];
+        for(int sn:s){
+            if(max < sn){
+                max = sn;
+                ans[0]++;
+            }
+            if(min > sn){
+                min = sn;
+                ans[1]++;
+            }
+        }
+        return ans;
+    }
 
 }
