62b37b184be5d4b159dd958889cfe063a956ecd5
devSryn <devseryun@gmail.com>
2019-05-17 17:32:55 +0900

    solved

--- src/HackerRank/BirthdayChocolate.java
+++ src/HackerRank/BirthdayChocolate.java
@@ -7,17 +7,22 @@ public class BirthdayChocolate {
 	// Complete the birthday function below.
 	static int birthday(List<Integer> s, int d, int m) {
 
-		int temp = 0;
+		int count = 0;
+		int size = s.size();
+		// int num = size - m + 1;
+		for (int i = 0; i < size - m + 1; i++) {
+			int sum = 0;
+			for (int j = i; j < i + m && j < size; j++) {
+				sum += s.get(i);
 
-		for (int i = 0, n = s.size(); i < n; i++) {
-			
-			
+			}
+
+			if (sum == d) {
+				count++;
+			}
 		}
 
-		for(Integer i: s){
-			
-		}
-		return temp;
+		return count;
 	}
 
 }
