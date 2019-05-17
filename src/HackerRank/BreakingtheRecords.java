1e1e4f267f2c711b017f6b302ed177aff7d14c3f
devSryn <devseryun@gmail.com>
2019-05-17 15:11:42 +0900

    solved

--- src/HackerRank/BreakingtheRecords.java
+++ src/HackerRank/BreakingtheRecords.java
@@ -2,21 +2,29 @@ package hackerRank;
 
 public class BreakingtheRecords {
 
-    static int[] getRecord(int[] s){
-        int max = s[0];
-        int min = s[0];
-        int ans[] = new int[2];
-        for(int sn:s){
-            if(max < sn){
-                max = sn;
-                ans[0]++;
-            }
-            if(min > sn){
-                min = sn;
-                ans[1]++;
-            }
-        }
-        return ans;
-    }
+	static int[] getRecord(int[] scores) {
+
+		int r[] = new int[2];
+
+		int min = scores[0];
+		int max = scores[0];
+		int minCount = 0;
+		int maxCount = 0;
+
+		for (int i = 1; i < scores.length; i++) {
+			if (scores[i] > max) {
+				maxCount++;
+				max = scores[i];
+			} else if (scores[i] < min) {
+				minCount++;
+				min = scores[i];
+			}
+		}
+
+		r[0] = maxCount;
+		r[1] = minCount;
+
+		return r;
+	}
 
 }
